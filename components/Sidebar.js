043c3828606b7a7299d3b869c382377924819a9d
Prajwal Adhikari <prajwalad101@gmail.com>
2021-12-12 16:22:26 +0545

    add side bar to the home page

---
+++ components/Sidebar.js
@@ -0,0 +1,54 @@
+import Image from 'next/image';
+import { HomeIcon } from '@heroicons/react/solid';
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  ClipboardListIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+} from '@heroicons/react/outline';
+
+import SidebarLink from './SidebarLink';
+
+function Sidebar() {
+  return (
+    <div className='hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full'>
+      <div className='flex items-center justify-center  w-14 h-14 hoverAnimation p-0 xl:ml-24'>
+        <Image src='https://rb.gy/ogau5a' alt='logo' width={30} height={30} />
+      </div>
+      <div className='space-y-2.5 mt-4 mb-2.5 xl:ml-24'>
+        <SidebarLink text='Home' Icon={HomeIcon} active />
+        <SidebarLink text='Explore' Icon={HashtagIcon} />
+        <SidebarLink text='Notifications' Icon={BellIcon} />
+        <SidebarLink text='Messages' Icon={InboxIcon} />
+        <SidebarLink text='Bookmarks' Icon={BookmarkIcon} />
+        <SidebarLink text='Lists' Icon={ClipboardListIcon} />
+        <SidebarLink text='Profile' Icon={UserIcon} />
+        <SidebarLink text='More' Icon={DotsCircleHorizontalIcon} />
+      </div>
+      {/* TWEET BUTTON */}
+      <button className='hidden xl:inline ml-24 bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]'>
+        Tweet
+      </button>
+      {/* USER COMPONENT */}
+      <div className='text-[#d9d9d9] flex items-center justify-center mt-auto hoverAnimation xl:ml-24 '>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src='https://i1.wp.com/www.followchain.org/wp-content/uploads/2021/09/best-discord-profile-pictures-9.png?w=256&ssl=1g'
+          alt=''
+          className='h-10 w-10 rounded-full xl:mr-2.5'
+        />
+        <div className='hidden xl:inline leading-5'>
+          <h4 className='font-bold'>Friday101</h4>
+          <p className='text-[#6e767d]'>@prajwal_ad</p>
+        </div>
+        <DotsHorizontalIcon className='h-5 hidden xl:inline ml-10' />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
