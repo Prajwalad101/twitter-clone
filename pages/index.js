1fc8e554d4d354e799e0d85cfcd974eb0e3f145d
Prajwal Adhikari <prajwalad101@gmail.com>
2021-12-13 16:15:05 +0545

    add next/auth

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { getProviders, getSession, useSession } from 'next-auth/react';
+
 import Head from 'next/head';
 import Feed from '../components/Feeds/Feed';
 import Sidebar from '../components/Sidebar/Sidebar';
@@ -16,4 +18,24 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const trendingResults = await fetch('https://jsonkeeper.com/b/NKEV').then(
+    (res) => res.json()
+  );
+  const followResults = await fetch('https://jsonkeeper.com/b/WWMJ').then(
+    (res) => res.json()
+  );
+  const providers = await getProviders();
+  const session = await getSession(context);
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  };
 }
